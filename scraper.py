f752970a17804a68e4e08cda1c3dfda7e9668314
Chloe S <csepulve@students.cs.ubc.ca>
2025-07-11 12:09:56 -0700

    retrieved html from ministries page

--- scraper.py
+++ scraper.py
@@ -1,1 +1,15 @@
-#test check
+# Source I referenced: GLOCAL-scraping-guide/glocal-scraping-guideline/scraping
+
+#following GLOCAL scraping guide
+import requests
+from bs4 import BeautifulSoup
+import json
+
+
+# retrieving raw HTML of "Ministries" page from official BC Government page
+response = requests.get("https://www2.gov.bc.ca/gov/content/governments/organizational-structure/ministries-organizations/ministries")
+response.raise_for_status()
+html = response.text
+
+#feeding soup my HTML and choosing BeautifulSoup parser
+soup = BeautifulSoup(html, "html.parser")
